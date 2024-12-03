7a567ab4745939af6b105713add01de134b729e1
Patricia <p.agudeloromero@gmail.com>
2024-12-03 16:29:09 +0800

    Update create_mmseqs2_db.py

--- create_viral_mmseqs2_nt_db/create_mmseqs2_db.py
+++ create_viral_mmseqs2_nt_db/create_mmseqs2_db.py
@@ -4,9 +4,9 @@ import os
 import argparse
 import subprocess
 
-"""
-Patricia Agudelo-Romero,PhD.
-"""
+#"""
+#Patricia Agudelo-Romero,PhD.
+#"""
 
 def create_directory(directory):
     """Create a directory if it does not exist."""
